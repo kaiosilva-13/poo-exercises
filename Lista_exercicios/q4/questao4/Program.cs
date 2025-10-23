32eb9de65d2099ce178542bab4f7cf594b843b5d
kaiosilva-13 <kaiovitor13052005@gmail.com>
2025-10-23 09:46:13 -0300

    Alterações arquivo Program.cs solução questao4

--- Lista_exercicios/q4/questao4/Program.cs
+++ Lista_exercicios/q4/questao4/Program.cs
@@ -5,8 +5,5 @@ class Program
     static void Main(string[] args)
     {
         ControleRemoto c1 = new ControleRemoto();
-        c1.LigarTV();
-        c1.MudarCanal(46);
-        c1.MostrarHistorico();
     }
 }
